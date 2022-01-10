129c7b91f894f3903321648899efc7a69ff62040
shail10 <shailkardani10@gmail.com>
2022-01-10 15:48:59 +0530

    Final Commit for Amazon

---
+++ Amazon/Column_name_from_a_given_column_number.cpp
@@ -0,0 +1,33 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution{
+    public:
+    string colName (long long int n)
+    {
+        string ans;
+        while(n>0){
+            char c = 'A' + (n-1)%26;
+            ans = c + ans;
+            n = (n-1)/26;
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        long long int n; cin >> n;
+        Solution ob;
+        cout << ob.colName (n) << '\n';
+    }
+}
+  // } Driver Code Ends
